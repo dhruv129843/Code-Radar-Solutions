87dfcf295e0d4ec347988de4ca5f7e809c25779d
dhruv129843 <dhruv0129.becse24@chitkara.edu.in>
2025-03-09 19:12:29 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,8 +1,8 @@
 // Your code here...
 def is_prime(n):
     if n <= 1:
-        return 0  # Numbers less than or equal to 1 are not prime
+        return 0  //Numbers less than or equal to 1 are not prime
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
-            return 0  # Found a divisor, hence not prime
-    return 1  # No divisors found, hence prime
+            return 0  // Found a divisor, hence not prime
+    return 1  // No divisors found, hence prime
