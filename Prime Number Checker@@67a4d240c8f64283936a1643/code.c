05605492527dacf4cee6bb05e8ef87eb5bb1bdd9
dhruv129843 <dhruv0129.becse24@chitkara.edu.in>
2025-03-10 18:46:47 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -3,8 +3,8 @@ int chkprimeno();
 int chkprimeno(){
     int t;
     if (t%2==0){
-        printf(1);}
-        else{printf(0);}
+        printf("1");}
+        else{printf("0");}
         return 0;
     
 
