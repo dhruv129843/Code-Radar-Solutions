178c02fd02e8c0946ea4f06b1aef0cafeea3eb76
dhruv129843 <dhruv0129.becse24@chitkara.edu.in>
2025-03-14 22:50:42 +0530

    Solution to Bubble Sort for Strings

--- Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
+++ Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
@@ -1,8 +1,8 @@
 // Your code here...
 char bubbleSort(char arr[], int n){
-    for(char ch='a';ch<='z'; ch++){
-    for(int i=0; i<n; i++){
-        if(ch[i]>ch[i+1]){
+    for(int pass=0;pass<n-1; pass++){
+    for(int i=0; i<n-1-pass; i++){
+        if(char[i]>char[i+1]){
             char temp= char[i];
             char[i]= char[i+1];
             char[i]=temp;
