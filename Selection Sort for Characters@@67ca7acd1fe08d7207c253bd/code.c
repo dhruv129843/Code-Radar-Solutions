f49c2f07484ea2e49bfc6fffde8ae0168d264182
dhruv129843 <dhruv0129.becse24@chitkara.edu.in>
2025-03-15 17:10:43 +0530

    Solution to Selection Sort for Characters

---
+++ Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
@@ -0,0 +1,17 @@
+// Your code here...
+selectionSort(int arr[],int n)
+{for (int i=0;i<n-1;i++){
+    for(int j=i+1;j<n;j++)
+    if( char temp= arr[j]; )
+    
+    (arr[j+1]<arr[j];)
+
+    (arr[j+1]=temp;)
+}}
+void printArray(int arr[], int n){
+    for(int j=0;j<n;j++){
+        printf("%d",arr[]);
+        printf("\n");
+    }
+
+}
